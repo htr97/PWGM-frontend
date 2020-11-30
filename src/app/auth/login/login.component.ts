99ed7446fc640f6b223264a7a98f54ccc81d65e3
htr97 <40081931+htr97@users.noreply.github.com>
2020-11-29 21:27:07 -0600

    Login_Register-Frontend

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { UserI } from '../../models/user';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private AuthService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  onLogin(form):void {
+    //console.log('Login',form.value)
+    this.AuthService.login(form.value).subscribe(res => {
+      this.router.navigateByUrl('/auth');
+    });
+  }
+
+}
