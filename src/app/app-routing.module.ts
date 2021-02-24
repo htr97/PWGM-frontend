107ca1180e93b2e3d0a9031eaca94a523d985205
htr97 <carmenreyes@unitec.edu>
2021-02-24 16:40:35 -0600

    Auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { InventoryFormComponent } from './inventory-form/inventory-form.component';
 import { InventoryViewComponent } from './inventory-view/inventory-view.component';
@@ -11,12 +13,20 @@ import { PrincipalComponent } from './principal/principal.component';
 const routes: Routes = [
   {path:'', redirectTo:'/auth/login', pathMatch:'full' },
   {path:'auth', loadChildren:'./auth/auth.module#AuthModule'},
-  {path:'principal',component:PrincipalComponent},
-  {path:'inventory',component:InventoryViewComponent},
-  {path: 'add-invItem',component:InventoryFormComponent},
-  {path: 'add-maintenance', component:MaintenanceFormComponent},
-  {path: 'maintenance-orders', component:MaintenanceOrdersComponent},
-  {path: 'home', component:HomeComponent}
+  {
+    path:'',
+    runGuardsAndResolvers:'always',
+    canActivate: [AuthGuard],
+    children: [
+    {path:'principal',component:PrincipalComponent},
+    {path:'inventory',component:InventoryViewComponent},
+    {path: 'add-invItem',component:InventoryFormComponent},
+    {path: 'add-maintenance', component:MaintenanceFormComponent},
+    {path: 'maintenance-orders', component:MaintenanceOrdersComponent},
+    {path: 'home', component:HomeComponent}
+    ]
+  },
+  {path: '**', redirectTo:'/auth/login', pathMatch: 'full'}
 ];
 
 @NgModule({
